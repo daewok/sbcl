72b518104fd76ab99aad46f4f74545301ddf4b52
Eric Timmons <etimmons@mit.edu>
2020-05-25 11:31:20 +0200

    Make all host steps before any target steps
    Patch originally by Manuel Giraud <manuel@ledu-giraud.fr>, I just cleaned up
    some comments.

--- make-host-1.sh
+++ make-host-1.sh
@@ -32,3 +32,16 @@ export LANG LC_ALL
 # environment.
 echo //building cross-compiler, and doing first genesis
 echo '(load "loader.lisp") (load-sbcl-file "make-host-1.lisp")' | $SBCL_XC_HOST
+
+# Use a little C program to grab stuff from the C header files and
+# smash it into Lisp source code.
+$GNUMAKE -C src/runtime clean
+$GNUMAKE -C src/runtime sbcl.h
+$GNUMAKE -C tools-for-build -I../src/runtime grovel-headers
+tools-for-build/grovel-headers > output/stuff-groveled-from-headers.lisp
+
+$GNUMAKE -C src/runtime after-grovel-headers
+
+if [ -n "$SBCL_HOST_LOCATION" ]; then
+    rsync -a output/stuff-groveled-from-headers.lisp "$SBCL_HOST_LOCATION/output"
+fi
